df59b8f3e7c98d34cef9f666b2ee59299032e314
x1.Mukesh <88231908+x1mukesh@users.noreply.github.com>
2021-10-02 20:49:49 +0530

    M print

--- m.js
+++ m.js
@@ -1,1 +1,13 @@
 
+var n = 11;
+        var center = parseInt(n / 2) ;
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                if (j == 0 || i == j && j <= center || j == n-1 - i && j >= center || j == n - 1) {
+                    document.write("#");
+                } else {
+                    document.write("_")
+                }
+            }
+            document.write("</br>")
+        }
