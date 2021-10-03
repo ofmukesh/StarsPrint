3222535f8eeaa851837beb7625fa391facf09b5e
x1.Mukesh <88231908+x1mukesh@users.noreply.github.com>
2021-10-04 00:59:12 +0530

    K Print

---
+++ K.js
@@ -0,0 +1,12 @@
+var n = 11;
+        var center = parseInt(n / 2) ;
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n-1; j++) {
+                if (j == 0 || j==center-i || i==center+j) {
+                    document.write("#");
+                } else {
+                    document.write("&nbsp&nbsp")
+                }
+            }
+            document.write("</br>")
+        }
